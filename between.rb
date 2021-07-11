17a89ec0667de8222746ff7654ce24c0cd75e1ae
paopapaopao <paolo.casugay@gmail.com>
2021-07-11 19:31:38 +0800

    Updated: converted to a function

--- between.rb
+++ between.rb
@@ -1,12 +1,14 @@
-puts "Enter a number: "
-num = gets.to_i
-
-if num < 0
-    puts "#{num} is less than 0"
-elsif num >= 0 && num <= 50
-    puts "#{num} is between 0 and 50"
-elsif num >= 51 && num <= 100
-    puts "#{num} is between 51 and 100"
-else
-    puts "#{num} is greater than 100"
+def between(x)
+	if x < 0
+		puts "#{x} is less than 0"
+	elsif x >= 0 && x <= 50
+		puts "#{x} is between 0 and 50"
+	elsif x >= 51 && x <= 100
+		puts "#{x} is between 51 and 100"
+	else
+		puts "#{x} is greater than 100"
+	end
 end
+
+puts "Enter a number:"
+between(gets.to_i)
