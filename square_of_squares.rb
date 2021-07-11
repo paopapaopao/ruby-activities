f19f604025900b3ec6cc901ed12824bfbe2e6caa
paopapaopao <paolo.casugay@gmail.com>
2021-07-11 19:31:02 +0800

    Updated: used built-in methods

--- square_of_squares.rb
+++ square_of_squares.rb
@@ -1,19 +1,19 @@
-def is_square(x)
-	if x < 0
+def is_square?(x)
+	if x.negative?
 		return false
 	end
 
-	for i in 0..x
-		if i * i == x
+	Array(0..x).each do |i|
+		if i ** 2 == x
 			return true
 		end
 	end
 	return false
 end
 
-puts "-1: #{is_square(-1)}"
-puts "0: #{is_square(0)}"
-puts "3: #{is_square(3)}"
-puts "4: #{is_square(4)}"
-puts "25: #{is_square(25)}"
-puts "26: #{is_square(26)}"
+puts "-1 #{is_square?(-1)}"
+puts "0 #{is_square?(0)}"
+puts "3 #{is_square?(3)}"
+puts "4 #{is_square?(4)}"
+puts "25 #{is_square?(25)}"
+puts "26 #{is_square?(26)}"
