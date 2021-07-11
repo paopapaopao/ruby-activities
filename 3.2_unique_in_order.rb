9ef63b67863d6f3c4afc2afc4534c3a4984dbb58
paopapaopao <paolo.casugay@gmail.com>
2021-07-11 19:41:38 +0800

    Deleted: unnecessary parentheses

--- 3.2_unique_in_order.rb
+++ 3.2_unique_in_order.rb
@@ -1,17 +1,17 @@
 def unique_in_order(sequence)
-	if (sequence.class != Array && sequence.class != String)
-		return (nil)
+	if sequence.class != Array && sequence.class != String
+		return nil
 	end
-	
+
 	array = sequence.class == Array ? sequence : sequence.split('')
 	unique = []
-	array.each_index do |index|
-		if (index == 0 || array[index] != array[index - 1])
-			unique << array[index]
+	array.each_index do |i|
+		if i == 0 || array[i] != array[i - 1]
+			unique << array[i]
 		end
 	end
 
-	return (unique)
+	return unique
 end
 
 p unique_in_order('AAAABBBCCDAABBB')
