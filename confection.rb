2d13836839f89a5fdce7e3dfece48134c530625d
paopapaopao <paolo.casugay@gmail.com>
2021-07-11 19:35:57 +0800

    Deleted: unnecessary parentheses

--- confection.rb
+++ confection.rb
@@ -1,20 +1,21 @@
 class Confection
-    def prepare()
-        return ("Baking at 350 degrees for 25 minutes.")
-    end
+	def prepare
+		puts "Baking at 350 degrees for 25 minutes."
+	end
 end
 
 class Cupcake < Confection
-    def prepare()
-        return (super + " Applying frosting.")
-    end
+	def prepare
+		super
+		puts "Applying frosting."
+	end
 end
 
 class BananaCake < Confection
 end
 
-cup_cake = Cupcake.new()
-banana_cake = BananaCake.new()
+cupcake = Cupcake.new
+banana_cake = BananaCake.new
 
-puts("Cupcake:\t" + cup_cake.prepare())
-puts("Banana cake:\t" + banana_cake.prepare())
+cupcake.prepare
+banana_cake.prepare
